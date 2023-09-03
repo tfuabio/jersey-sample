5c0de245c963ec095f379146c2905961e2cd2b0e
tfuabio <tfuabio@gmail.com>
2023-09-03 01:12:46 +0000

    add config

--- src/main/java/com/sample/jersey/app/Config.java
+++ src/main/java/com/sample/jersey/app/Config.java
@@ -1,5 +1,52 @@
 package com.sample.jersey.app;
 
-public class Config {
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import com.google.gson.Gson;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Config {
+  private Person person = new Person();
+
+  @Getter
+  @ToString
+  private static class Person {
+    private String name = "Hanako Yamada";
+    private Integer age = 31;
+  }
+
+  private static Config config;
+
+  public static synchronized void loadFromFile(Path path) {
+    if (config == null) {
+      // Initiallize.
+      config = new Config();
+
+      // Load Config File.
+      try (Reader reader = Files.newBufferedReader(path)) {
+        config = new Gson().fromJson(reader, Config.class);
+      } catch (IOException e) {
+        // TODO: handle exception
+      }
+    }
+  }
+
+  public static Config get() {
+    return config;
+  }
+
+  // 動作確認用
+  public static void main(String[] args) {
+    Config.loadFromFile(Paths.get("./config.json"));
+    System.out.println(Config.get());
+  }
 }
